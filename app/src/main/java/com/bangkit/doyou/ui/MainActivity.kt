f96226a9d87151130ce1451c91c68aaf665fb381
Syahriza <riza29.indra@gmail.com>
2024-06-15 21:12:20 +0700

    Update slicing

--- app/src/main/java/com/bangkit/doyou/ui/MainActivity.kt
+++ app/src/main/java/com/bangkit/doyou/ui/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.bangkit.doyou.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bangkit.doyou.R
+import com.bangkit.doyou.ui.profile.ProfileActivity
+import com.bangkit.doyou.ui.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +22,25 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        // for navbar intent
+        val goHome: ImageButton = findViewById(R.id.home_btn)
+        goHome.setOnClickListener {
+            val intentHome = Intent(this@MainActivity, MainActivity::class.java)
+            startActivity(intentHome)
+        }
+
+        val goProfile: ImageButton = findViewById(R.id.btn_user)
+        goProfile.setOnClickListener {
+            val intentProfile = Intent(this@MainActivity, ProfileActivity::class.java)
+            startActivity(intentProfile)
+        }
+
+        val goSearch: ImageButton = findViewById(R.id.search_btn)
+        goSearch.setOnClickListener {
+            val intentSearch = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intentSearch)
+        }
 
      }
 }
