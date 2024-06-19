57d8fdae510846a0604598ebe45c46138d66895f
Syahriza <riza29.indra@gmail.com>
2024-06-19 15:52:36 +0700

    Update slicing

---
+++ app/src/main/java/com/bangkit/doyou/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,33 @@
+package com.bangkit.doyou.ui.welcome
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bangkit.doyou.R
+import com.bangkit.doyou.ui.login.LoginActivity
+import com.bangkit.doyou.ui.signup.SignUpActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        //intent
+        val goLogin: Button = findViewById(R.id.to_login)
+        goLogin.setOnClickListener {
+            val intentLogin = Intent (this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intentLogin)
+        }
+
+        val goSignup: Button = findViewById(R.id.to_signup)
+        goSignup.setOnClickListener {
+            val intentSignUp = Intent (this@WelcomeActivity, SignUpActivity::class.java)
+            startActivity(intentSignUp)
+        }
+
+        }
+   }
